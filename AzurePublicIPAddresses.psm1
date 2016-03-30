0a8c3bb1c5f198d6c2eb3d6e15e6e3fb6f56ba1e
Kieran Jacobsen <code@poshsecurity.com>
2016-03-31 00:24:48 +1100

    adding the auto loading psm1

---
+++ AzurePublicIPAddresses.psm1
@@ -0,0 +1,18 @@
+﻿#
+# Export the module members - KUDOS to the chocolatey project for this efficent code
+# 
+
+
+#get the path of where the module is saved (if module is at c:\myscripts\module.psm1, then c:\myscripts\)
+$mypath = (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)
+
+#find all the ps1 files in the subfolder functions
+Resolve-Path -Path $mypath\functions\*.ps1 | ForEach-Object -Process {
+    . $_.ProviderPath
+    $Function = ((Split-Path -path $_.ProviderPath -Leaf).Split('.')[0])
+    Export-ModuleMember -Function $function
+}
+
+#
+# Define any alias and export them - Kieran Jacobsen
+#
