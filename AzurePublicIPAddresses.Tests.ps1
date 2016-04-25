2a9c70f6f67fae8df5527e8e58d4c13dbca0893a
Kieran Jacobsen <code@poshsecurity.com>
2016-04-25 20:45:14 +1000

    Adding Pester

---
+++ AzurePublicIPAddresses.Tests.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module $PSScriptRoot\AzurePublicIPAddresses.psm1 -Force
+
+Describe 'AzurePublicIPAddresses' {
+    Context 'Scrypt Analyzer' {
+        It 'Does not have any issues with the Script Analyser - Get-MicrosoftAzureDatacenterIPRange' {
+            Invoke-ScriptAnalyzer .\Functions\Get-MicrosoftAzureDatacenterIPRange.ps1 | Should be $null
+        }
+        It 'Does not have any issues with the Script Analyser - Get-MicrosoftAzureDatacenterIPRangeFile' {
+            Invoke-ScriptAnalyzer .\Functions\Get-MicrosoftAzureDatacenterIPRangeFile.ps1 | Should be $null
+        }
+    }
+}
