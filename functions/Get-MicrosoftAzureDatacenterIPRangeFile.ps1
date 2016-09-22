d53deda3799161f4ccb289c0f3102b33ddb4af6a
Kieran Jacobsen <code@poshsecurity.com>
2016-09-22 17:12:24 +1000

    fixed tabbing

--- functions/Get-MicrosoftAzureDatacenterIPRangeFile.ps1
+++ functions/Get-MicrosoftAzureDatacenterIPRangeFile.ps1
@@ -1,29 +1,32 @@
-function Get-MicrosoftAzureDatacenterIPRangeFile {
+#requires -Version 3.0
+
+function Get-MicrosoftAzureDatacenterIPRangeFile 
+{
     <#
-        .SYNOPSIS
-        Downloads the Microsoft Azure Datacenter IP Ranges file
-        .DESCRIPTION
-        The Get-MicrosoftAzureDatacenterIPRangeFile cmdlet will download the Microsoft Azure Datacenter IP Ranges file from the Microsoft Downloads site (https://www.microsoft.com/en-us/download/details.aspx?id=41653).
+            .SYNOPSIS
+            Downloads the Microsoft Azure Datacenter IP Ranges file
+            .DESCRIPTION
+            The Get-MicrosoftAzureDatacenterIPRangeFile cmdlet will download the Microsoft Azure Datacenter IP Ranges file from the Microsoft Downloads site (https://www.microsoft.com/en-us/download/details.aspx?id=41653).
         
-        It should be noted that this file is updated on a weekly basis, so if you save this file, then you should re-download this file on a regular basis.
+            It should be noted that this file is updated on a weekly basis, so if you save this file, then you should re-download this file on a regular basis.
         
-        This cmdlet makes use of Invoke-WebRequest.
-        .EXAMPLE
-        C:\PS> Get-MicrosoftAzureDatacenterIPRangeFile
-        Returns an XML document from the Microsoft Azure Datacenter IP Ranges file
-        .EXAMPLE
-        C:\PS> Get-MicrosoftAzureDatacenterIPRangeFile -Path C:\Temp\AzureRanges.xml
-        Dowloads the Microsoft Azure Datacenter IP Ranges file to C:\Temp\AzureRanges.xml
-        .OUTPUTS
-        XML document containing Azure Subnets, or
-        Void if saving the file to the file system
+            This cmdlet makes use of Invoke-WebRequest.
+            .EXAMPLE
+            C:\PS> Get-MicrosoftAzureDatacenterIPRangeFile
+            Returns an XML document from the Microsoft Azure Datacenter IP Ranges file
+            .EXAMPLE
+            C:\PS> Get-MicrosoftAzureDatacenterIPRangeFile -Path C:\Temp\AzureRanges.xml
+            Dowloads the Microsoft Azure Datacenter IP Ranges file to C:\Temp\AzureRanges.xml
+            .OUTPUTS
+            XML document containing Azure Subnets, or
+            Void if saving the file to the file system
     #>
     [CmdletBinding(DefaultParameterSetName = 'xml')]
-    [OutputType([System.Xml.XmlDocument], ParameterSetName='path')]
-    [OutputType([void], ParameterSetName='xml')]
+    [OutputType([System.Xml.XmlDocument], ParameterSetName = 'path')]
+    [OutputType([void], ParameterSetName = 'xml')]
     param(
         # Path where you want to save the XML file
-        [Parameter(Mandatory = $false, ParameterSetName='path', Position = 0)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'path', Position = 0)]
         [String]
         $Path
     )
